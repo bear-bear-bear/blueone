0c0207f7b1ebc34c5239e7e809b7c47e00c78818
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-19 07:22:20 +0900

    feat: 서브셋 폰트 추가, global 스타일 설정

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from 'next/app';
+import GlobalStyles from '@globalStyles';
 import 'antd/dist/antd.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
