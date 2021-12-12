350bb65360687e8c344b95f744282229921e3bbc
bearbearbear <ghlwkdthfl@gmail.com>
2021-12-12 18:42:10 +0900

    feat: 미들웨어 환경에 따른 분기 및 설정

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,13 +1,58 @@
 import express, { Request, Response } from 'express';
-// import path from 'path';
-// import session from 'express-session';
-// import cookieParser from 'cookie-parser';
-// import cors from 'cors';
-// import morgan from 'morgan';
-// import hpp from 'hpp';
-// import helmet from 'helmet';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import hpp from 'hpp';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1);
+  app.use(morgan('combined'));
+  app.use(hpp());
+  app.use(helmet());
+  app.use(
+    cors({
+      origin: ['https://example.com'],
+      credentials: true,
+    }),
+  );
+  app.use(
+    session({
+      saveUninitialized: false,
+      resave: false,
+      secret: process.env.COOKIE_SECRET as string,
+      proxy: true,
+      cookie: {
+        httpOnly: true,
+        secure: true,
+        domain: '.example.com',
+      },
+    }),
+  );
+} else {
+  app.use(morgan('dev'));
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    }),
+  );
+  app.use(
+    session({
+      saveUninitialized: false,
+      resave: false,
+      secret: process.env.COOKIE_SECRET as string,
+    }),
+  );
+}
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.get('/welcome', (req: Request, res: Response) => {
   res.send('welcome!');
