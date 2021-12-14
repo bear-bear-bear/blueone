117e5f7cc891d5e039bdd0669927e3aaedf783d6
bearbearbear <ghlwkdthfl@gmail.com>
2021-12-14 09:00:49 +0900

    feat: DB 생성 로직을 config에서 분리

--- back/src/config/config.ts
+++ back/src/config/config.ts
@@ -1,9 +1,5 @@
-import dotenv from 'dotenv';
 import type { Dialect } from 'sequelize';
-import { sync as rm } from 'del';
-import path from 'path';
-import { mkdir, writeFile } from 'fs';
-import { execSync } from 'child_process';
+import dotenv from 'dotenv';
 dotenv.config();
 
 type NodeEnv = 'development' | 'production';
@@ -46,32 +42,3 @@ if (isUndefinedValue) {
 }
 
 export default config[nodeEnv];
-
-/**
- * @desc 임의의 json 을 생성하여 npx sequelize db:create 명령어를 수행합니다.
- */
-const createDatabase = () => {
-  const rootDir = path.join(__dirname, '..', '..'); // 🚩 High risk, because path is relative
-  const sequelizeConfigJson = JSON.stringify(config[nodeEnv]);
-  const sequelizeConfigDirname = path.join(rootDir, 'config');
-
-  rm(sequelizeConfigDirname);
-
-  mkdir(sequelizeConfigDirname, (err) => {
-    if (err) throw err;
-    writeFile(
-      path.join(sequelizeConfigDirname, 'config.json'),
-      sequelizeConfigJson,
-      (error) => {
-        if (error) throw error;
-        execSync('npx sequelize-cli db:create');
-        rm(sequelizeConfigDirname);
-        console.log('데이터베이스 생성완료');
-      },
-    );
-  });
-};
-
-if (process.argv.includes('--create')) {
-  createDatabase();
-}
