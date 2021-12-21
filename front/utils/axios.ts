db0e46d87832a900780b18b92c8d811acf234ae7
bearbearbear <ghlwkdthfl@gmail.com>
2021-12-21 13:13:55 +0900

    feat: 간단한 axios 인스턴스 작성

--- front/utils/axios.ts
+++ front/utils/axios.ts
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
-const customAxios = axios.create({
+const httpClient = axios.create({
   baseURL: 'http://localhost:8001',
 });
+
+httpClient.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.resolve({ error }),
+);
+
+export default httpClient;
