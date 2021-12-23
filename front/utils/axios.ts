a8fed8a6bd01c285dd3b4303b7306bf3bb980176
bearbearbear <ghlwkdthfl@gmail.com>
2021-12-23 23:08:31 +0900

    feat: 쿠키 세팅 옵션 추가

--- front/utils/axios.ts
+++ front/utils/axios.ts
@@ -2,11 +2,7 @@ import axios from 'axios';
 
 const httpClient = axios.create({
   baseURL: 'http://localhost:8001',
+  withCredentials: true,
 });
-
-httpClient.interceptors.response.use(
-  (response) => response,
-  (error) => Promise.resolve({ error }),
-);
 
 export default httpClient;
