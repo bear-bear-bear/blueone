d97c6dfec2dbcd18fd9e61512b64665f23212cdc
bearbearbear <ghlwkdthfl@gmail.com>
2021-12-23 15:36:19 +0900

    feat: 임시 어드민 생성 폼 작성

--- front/pages/temp.tsx
+++ front/pages/temp.tsx
@@ -33,57 +33,119 @@ const TempUserCreatePage: NextPage = () => {
     [],
   );
 
+  const handleAdminSubmit: FormEventHandler<HTMLFormElement> = useCallback(async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    try {
+      const admin = await httpClient
+        .post('/users/admin', {
+          phoneNumber: formData.get('phoneNumber')
+        })
+        .then((res) => res.data);
+      console.log('admin 생성완료:', admin);
+    } catch (err) {
+      console.error(err);
+    }
+  }, [])
+
   return (
-    <form
-      action=""
-      onSubmit={handleSubmit}
+    <div
       style={{
-        width: 'fit-content',
-        padding: '1.33rem',
+        display: 'flex',
+        padding: '1rem',
+        border: '1px solid black',
       }}
     >
-      {textFields.map((field, i) => (
-        <div key={field + i}>
-          <label
-            htmlFor={field}
+      <div>
+        <h1>유저 생성</h1>
+        <form
+          action=""
+          onSubmit={handleSubmit}
+          style={{
+            width: 'fit-content',
+            padding: '1.33rem',
+          }}
+        >
+          {textFields.map((field, i) => (
+            <div key={field + i}>
+              <label
+                htmlFor={field}
+                style={{
+                  width: '200px',
+                  display: 'inline-block',
+                  marginRight: '1rem',
+                  textAlign: 'right',
+                }}
+              >
+                {`${field}: `}
+              </label>
+              <input type="text" id={field + i} name={field} required />
+            </div>
+          ))}
+          {dateFields.map((field, i) => (
+            <div key={field + i}>
+              <label
+                htmlFor={field}
+                style={{
+                  width: '200px',
+                  display: 'inline-block',
+                  marginRight: '1rem',
+                  textAlign: 'right',
+                }}
+              >
+                {`${field}: `}
+              </label>
+              <input type="date" id={field + i} name={field} required />
+            </div>
+          ))}
+          <button
+            type="submit"
             style={{
-              width: '200px',
-              display: 'inline-block',
-              marginRight: '1rem',
-              textAlign: 'right',
+              marginTop: '1rem',
+              float: 'right',
             }}
           >
-            {`${field}: `}
-          </label>
-          <input type="text" id={field + i} name={field} required />
-        </div>
-      ))}
-      {dateFields.map((field, i) => (
-        <div key={field + i}>
-          <label
-            htmlFor={field}
+            유저 생성
+          </button>
+        </form>
+      </div>
+      <div>
+        <h1>어드민 생성</h1>
+        <form
+          action=""
+          onSubmit={handleAdminSubmit}
+          style={{
+            width: 'fit-content',
+            padding: '1.33rem',
+          }}
+        >
+          <div>
+            <label
+              htmlFor="phoneNumber"
+              style={{
+                width: '200px',
+                display: 'inline-block',
+                marginRight: '1rem',
+                textAlign: 'right',
+              }}
+            >
+              phoneNumber:
+            </label>
+            <input type="text" id="phoneNumber" name="phoneNumber" required />
+          </div>
+          <button
+            type="submit"
             style={{
-              width: '200px',
-              display: 'inline-block',
-              marginRight: '1rem',
-              textAlign: 'right',
+              marginTop: '1rem',
+              float: 'right',
             }}
           >
-            {`${field}: `}
-          </label>
-          <input type="date" id={field + i} name={field} required />
-        </div>
-      ))}
-      <button
-        type="submit"
-        style={{
-          marginTop: '1rem',
-          float: 'right',
-        }}
-      >
-        유저 생성
-      </button>
-    </form>
+            어드민 생성
+          </button>
+        </form>
+      </div>
+    </div>
   );
 };
 
