b12614023ec4b278e134959497155655a4be7487
bearbearbear <ghlwkdthfl@gmail.com>
2021-12-27 23:15:23 +0900

    feat: swr global config

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,13 +1,20 @@
 import type { AppProps } from 'next/app';
+import { SWRConfig } from 'swr';
 import GlobalStyles from '@globalStyles/index';
 import 'antd/dist/antd.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SWRConfig
+      value={{
+        errorRetryCount: 3,
+        dedupingInterval: 2000,
+        errorRetryInterval: 5000,
+      }}
+    >
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </SWRConfig>
   );
 }
 
