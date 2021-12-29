bee251c5062a5a01edf54dad2f7779bba87adeb3
bearbearbear <ghlwkdthfl@gmail.com>
2021-12-29 17:14:59 +0900

    feat: 기사 관리 탭을 하나의 섹션으로 관리하도록 변경

--- front/components/AdminContent/index.tsx
+++ front/components/AdminContent/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import WorkTable from './WorkTable';
 import WorkAddForm from '@components/AdminContent/WorkAddForm';
 
-export type ContentTitle = '업무 목록' | '업무 추가' | '기사 목록' | '기사 추가' | '공지사항';
+export type ContentTitle = '업무 목록' | '업무 추가' | '기사 관리' | '공지사항';
 export type Content = {
   title: ContentTitle;
   component: FC;
@@ -18,11 +18,7 @@ const contentList: Content[] = [
     component: WorkAddForm,
   },
   // {
-  //   title: '기사 목록',
-  //   component: () => <div>임시 컨텐츠</div>,
-  // },
-  // {
-  //   title: '기사 추가',
+  //   title: '기사 관리',
   //   component: () => <div>임시 컨텐츠</div>,
   // },
   // {
--- front/components/AdminLayout/index.tsx
+++ front/components/AdminLayout/index.tsx
@@ -54,10 +54,9 @@ const AdminLayout: FC = () => {
             <Menu.Item key="업무 추가">업무 추가</Menu.Item>
           </SubMenu>
           <Menu.Divider style={{ backgroundColor: '#000810' }} />
-          <SubMenu key="기사 관리" icon={<TeamOutlined />} title="기사 관리">
-            <Menu.Item key="기사 목록">기사 목록</Menu.Item>
-            <Menu.Item key="기사 추가">기사 추가</Menu.Item>
-          </SubMenu>
+          <Menu.Item key="기사 관리" icon={<TeamOutlined />} title="기사 관리">
+            기사 관리
+          </Menu.Item>
           <Menu.Divider style={{ backgroundColor: '#000810' }} />
           <Menu.Item key="공지사항" icon={<NotificationOutlined />} title="공지사항">
             공지사항
