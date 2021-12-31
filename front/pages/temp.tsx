e2818ded09c4fc1091280c0fa1586ea1c49dbf15
bearbearbear <ghlwkdthfl@gmail.com>
2021-12-31 11:13:32 +0900

    feat: 임시 페이지 권한 해제

--- front/pages/temp.tsx
+++ front/pages/temp.tsx
@@ -2,45 +2,8 @@ import type { NextPage } from 'next';
 import { useCallback } from 'react';
 import type { FormEventHandler } from 'react';
 import httpClient from '@utils/axios';
-import type { EndPoint } from '@typings';
-import useAdmin from '@hooks/useAdmin';
-
-const textFields = [
-  'phoneNumber',
-  'realname',
-  'residentRegistrationNumber',
-  'licenseNumber',
-  'licenseType',
-  'insuranceNumber',
-];
-
-const dateFields = ['insuranceExpirationDate'];
 
 const TempUserCreatePage: NextPage = () => {
-  const { isAdminLoggedIn } = useAdmin({
-    redirectTo: '/login',
-  });
-
-  const handleSubmit: FormEventHandler<HTMLFormElement> = useCallback(async (e) => {
-    e.preventDefault();
-
-    const reqBody = Array.from(e.currentTarget.querySelectorAll('input')).reduce(
-      (acc, inputEl) => ({
-        ...acc,
-        [inputEl.name]: inputEl.value,
-      }),
-      {} as EndPoint['POST /users']['requestBody'],
-    );
-    try {
-      const createdUser = await httpClient
-        .post<EndPoint['POST /users']['responses']['202']>('/users', reqBody)
-        .then((res) => res.data);
-      console.log('createdUser', createdUser);
-    } catch (err) {
-      console.error(err);
-    }
-  }, []);
-
   const handleAdminSubmit: FormEventHandler<HTMLFormElement> = useCallback(async (e) => {
     e.preventDefault();
 
@@ -57,7 +20,6 @@ const TempUserCreatePage: NextPage = () => {
     }
   }, []);
 
-  if (!isAdminLoggedIn) return null;
   return (
     <div
       style={{
@@ -66,59 +28,6 @@ const TempUserCreatePage: NextPage = () => {
         border: '1px solid black',
       }}
     >
-      <div>
-        <h1>유저 생성</h1>
-        <form
-          action=""
-          onSubmit={handleSubmit}
-          style={{
-            width: 'fit-content',
-            padding: '1.33rem',
-          }}
-        >
-          {textFields.map((field, i) => (
-            <div key={field + i}>
-              <label
-                htmlFor={field}
-                style={{
-                  width: '200px',
-                  display: 'inline-block',
-                  marginRight: '1rem',
-                  textAlign: 'right',
-                }}
-              >
-                {`${field}: `}
-              </label>
-              <input type="text" id={field + i} name={field} required />
-            </div>
-          ))}
-          {dateFields.map((field, i) => (
-            <div key={field + i}>
-              <label
-                htmlFor={field}
-                style={{
-                  width: '200px',
-                  display: 'inline-block',
-                  marginRight: '1rem',
-                  textAlign: 'right',
-                }}
-              >
-                {`${field}: `}
-              </label>
-              <input type="date" id={field + i} name={field} required />
-            </div>
-          ))}
-          <button
-            type="submit"
-            style={{
-              marginTop: '1rem',
-              float: 'right',
-            }}
-          >
-            유저 생성
-          </button>
-        </form>
-      </div>
       <div>
         <h1>어드민 생성</h1>
         <form
