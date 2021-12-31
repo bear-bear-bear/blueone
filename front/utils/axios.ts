c0c5478624535b2dc07cefb42b893ce7ebae4fe2
bearbearbear <ghlwkdthfl@gmail.com>
2021-12-31 10:14:59 +0900

    feat: nginx 적용

--- front/utils/axios.ts
+++ front/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://blueone.app' : 'http://localhost:80',
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://blueone.app' : 'http://localhost:8001',
   withCredentials: true,
 });
 
