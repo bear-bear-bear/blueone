7a3c7ee93186428cb6f6de492d2e02048cd7887c
bearbearbear <ghlwkdthfl@gmail.com>
2022-01-01 10:13:58 +0900

    build

--- front/utils/axios.ts
+++ front/utils/axios.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-  baseURL: 'http://localhost:8001',
-  // baseURL: process.env.NODE_ENV === 'production' ? 'https://blueone.app' : 'http://localhost:8001',
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://blueone.app' : 'http://localhost:8001',
   withCredentials: true,
 });
 
