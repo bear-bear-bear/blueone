c4771821b7d41fe82ad75d2a00dd3598ee122e0d
bearbearbear <ghlwkdthfl@gmail.com>
2022-01-03 14:49:38 +0900

    feat: 등록 한글 정규식 안되는 문제 해결

--- front/utils/regex.ts
+++ front/utils/regex.ts
@@ -1,6 +1,6 @@
 export default {
   phoneNumber: /^\d{7,20}$/,
   dateOfBirth: /^\d{6}$/,
-  licenseNumber: /^[\d-가-힣ㄱ-ㅎ]{1,32}$/,
-  insuranceNumber: /^[\d-]{1,32}$/,
+  licenseNumber: /^[-\d가-힣ㄱ-ㅎ]{1,32}$/,
+  insuranceNumber: /^[-\d]{1,32}$/,
 };
