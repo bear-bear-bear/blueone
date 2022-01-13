93fbf2f62078cd1e10965a86c9770282ee532ccf
bearbearbear <ghlwkdthfl@gmail.com>
2022-01-14 00:37:34 +0900

    chore: 버튼 default로 변경

--- front/components/Admin/content/NoticeBoard/AddButton.tsx
+++ front/components/Admin/content/NoticeBoard/AddButton.tsx
@@ -24,7 +24,7 @@ const AddButton: FC<Props> = ({ swrKey }) => {
 
   return (
     <>
-      <Button type="primary" onClick={handleEditIconClick}>
+      <Button type="default" onClick={handleEditIconClick}>
         등록
       </Button>
       <Modal
