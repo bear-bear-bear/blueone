80c15c7863b07b76709b30b236c757a12b9cc1ed
Will Kim <will@chequer.io>
2022-04-24 21:28:50 +0900

    feat: 마이그레이션 실행 스크립트 추가

---
+++ back/src/config/exec.ts
@@ -0,0 +1,9 @@
+import sequelizeCli from '@/config/sequelizeCli';
+
+if (process.argv.includes('--create')) {
+  sequelizeCli('db:create', '데이터베이스 생성 완료');
+}
+
+if (process.argv.includes('--migrate')) {
+  sequelizeCli('db:migrate', '데이터베이스 마이그레이션 완료');
+}
---
+++ back/src/config/sequelizeCli.ts
@@ -0,0 +1,26 @@
+import path from 'path';
+import { sync as rm } from 'del';
+import { mkdir, writeFile } from 'fs';
+import { execSync } from 'child_process';
+import config from '@/config/config';
+
+/**
+ * @desc 임의의 json 을 생성하여 sequelize-cli 명령어를 수행합니다.
+ */
+export default function sequelizeCli(command: string, doneLog: string) {
+  const rootDir = path.join(__dirname, '..', '..'); // 🚩 High risk, because path is relative
+  const json = JSON.stringify(config);
+  const dirname = path.join(rootDir, 'config');
+
+  rm(dirname);
+
+  mkdir(dirname, (err) => {
+    if (err) throw err;
+    writeFile(path.join(dirname, 'config.json'), json, (error) => {
+      if (error) throw error;
+      execSync(`npx sequelize-cli ${command}`);
+      rm(dirname);
+      console.log(doneLog);
+    });
+  });
+}
