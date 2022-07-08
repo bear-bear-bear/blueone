5801ff614bb44d849a7c1394a05491391306d8ab
galaxy4276 <galaxyhi4276@gmail.com>
2022-07-08 22:31:21 +0900

    feat: 예약 관련 기능 유틸 함수 작성

---
+++ back/src/utils/query/work.ts
@@ -0,0 +1,58 @@
+import { User, UserInfo, Work } from '@/models';
+import { Op } from 'sequelize';
+
+/**
+ * @param gt string 형식의 날짜 데이터입니다. ( greater then )
+ * @param lt string 형식의 날짜 데이터입니다. ( less then )
+ * @param booked 예약된 작업 정보를 표시할ㅈ 지에 대한 bool 값입니다.
+ * @description 날짜 데이터를 받아 해당하는 날짜 범위의 예약 작업 또는 작업 데이터를 조회하여 반환합니다.
+ */
+export const getWorksByConditionallyAsBooking = (
+  gt: string,
+  lt: string,
+  booked = false
+) => {
+  const bookingField = booked ? {
+    bookingDate: {
+      [Op.ne]: null,
+    }
+  } : {
+    bookingDate: {
+      [Op.eq]: null,
+    }
+  };
+
+  return Work.findAll({
+    where: {
+      createdAt: {
+        [Op.gt]: gt,
+        [Op.lt]: lt,
+      },
+      ...bookingField,
+    },
+    order: [['createdAt', 'DESC']],
+    include: [
+      {
+        model: User,
+        attributes: {
+          exclude: ['password'],
+        },
+        include: [
+          {
+            model: UserInfo,
+            attributes: ['realname'],
+          },
+        ],
+      },
+    ],
+  });
+};
+
+/**
+ * @description work 관련 Sequelize Query 에서 기본 Where 옵션을 부여합니다.
+ */
+export const spreadDefaultWhereParamsQueriedByWork = () => ({
+  bookingDate: {
+    [Op.eq]: null,
+  }
+})
