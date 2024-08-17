695ecc4ef7789a86a9fee66e6f23a65f86522f6a
Will Kim <will@chequer.io>
2024-08-17 20:23:53 +0900

    refactor(front): Menu children deprecated 처리되어 items로 변경

--- packages/blueone-front/src/app/contractor/layout.tsx
+++ packages/blueone-front/src/app/contractor/layout.tsx
@@ -1,77 +1,42 @@
 'use client';
 import { useState, ReactNode } from 'react';
-import { Button, Layout, Menu, MenuProps, Tooltip } from 'antd';
+import { Button, Layout, Menu, Tooltip } from 'antd';
 import Image from 'next/image';
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { useSignOut } from '@/features/sign-out';
 import { LogoutOutlined } from '@ant-design/icons';
-import navItems, { getTitleByRoute } from './nav-items';
+import navItems, { getMenuKeyByPathname, getPageHeadingByPathname, allGroupKeys } from './nav-items';
 
 const { Content: Main, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
 
 export default function ContractorLayout({ children }: { children: ReactNode }) {
-  const router = useRouter();
   const pathname = usePathname();
   const { mutate: signOut } = useSignOut();
   const [collapsed, setCollapsed] = useState(false);
 
-  const handleClickMenu: MenuProps['onClick'] = (e) => {
-    router.push(e.key);
-  };
-
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed} breakpoint="lg">
+      <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed} breakpoint="lg" width={240}>
         <div className="min-h-12 flex justify-center p-4">
           {collapsed ? (
             <Image src="/icon_white.svg" alt="로고" width={20} height={20} />
           ) : (
-            <Image src="/logo_white.svg" alt="로고" width={100} height={20} />
+            <Image src="/logo_white.svg" alt="로고" width={110} height={20} />
           )}
         </div>
 
         <Menu
           theme="dark"
-          defaultOpenKeys={['업무 관리']} // SubMenu
-          selectedKeys={[pathname]} // MenuItem
-          onClick={handleClickMenu}
+          defaultOpenKeys={allGroupKeys}
+          defaultSelectedKeys={[getMenuKeyByPathname(pathname)]}
           mode="inline"
-          inlineIndent={16}
-        >
-          <Divider />
-
-          {navItems.map((page) => {
-            if ('children' in page) {
-              return (
-                <>
-                  <SubMenu key={page.title} icon={page.icon} title={page.title}>
-                    {page.children.map((child) => (
-                      <Menu.Item key={child.route} title={child.title}>
-                        {child.title}
-                      </Menu.Item>
-                    ))}
-                  </SubMenu>
-                  <Divider />
-                </>
-              );
-            }
-
-            return (
-              <>
-                <Menu.Item key={page.route} icon={page.icon} title={page.title}>
-                  {page.title}
-                </Menu.Item>
-                <Divider />
-              </>
-            );
-          })}
-        </Menu>
+          items={navItems}
+        />
       </Sider>
 
       <Layout>
         <Layout.Header className="flex justify-between items-center px-4 text-white">
-          <h1>{getTitleByRoute(pathname)}</h1>
+          <h1>{getPageHeadingByPathname(pathname)}</h1>
 
           <Tooltip title="로그아웃">
             <Button
@@ -89,8 +54,4 @@ export default function ContractorLayout({ children }: { children: ReactNode }) 
       </Layout>
     </Layout>
   );
-}
-
-function Divider() {
-  return <Menu.Divider className="bg-gray-950 border-gray-950 text-gray-950" />;
 }
--- packages/blueone-front/src/app/contractor/nav-items.tsx
+++ packages/blueone-front/src/app/contractor/nav-items.tsx
@@ -1,50 +1,63 @@
+import { MenuProps } from 'antd';
+import { ItemType, MenuItemGroupType } from 'antd/es/menu/interface';
+import Link from 'next/link';
 import { CarOutlined, NotificationOutlined, TeamOutlined } from '@ant-design/icons';
 
-const navItems = [
+const routeToLabelMap: Record<string, string> = {
+  '/contractor/works': '업무 목록',
+  '/contractor/add-work-for-mobile': '업무 등록',
+  '/contractor/subcontractors': '기사 관리',
+  '/contractor/notices': '공지사항',
+};
+
+type MenuItem = Required<MenuProps>['items'][number];
+const navItems: MenuItem[] = [
   {
-    title: '업무 관리',
+    type: 'divider',
+  },
+  {
+    key: '업무 관리',
+    label: '업무 관리',
     icon: <CarOutlined />,
     children: [
       {
-        title: '업무 목록',
-        route: '/contractor/works',
+        key: routeToLabelMap['/contractor/works'],
+        label: <Link href="/contractor/works">{routeToLabelMap['/contractor/works']}</Link>,
       },
       {
-        title: '업무 등록',
-        route: '/contractor/add-work-for-mobile',
+        key: routeToLabelMap['/contractor/add-work-for-mobile'],
+        label: <Link href="/contractor/add-work-for-mobile">{routeToLabelMap['/contractor/add-work-for-mobile']}</Link>,
       },
     ],
   },
   {
-    title: '기사 관리',
-    route: '/contractor/subcontractors',
+    type: 'divider',
+  },
+  {
+    key: routeToLabelMap['/contractor/subcontractors'],
+    label: <Link href="/contractor/subcontractors">{routeToLabelMap['/contractor/subcontractors']}</Link>,
     icon: <TeamOutlined />,
   },
   {
-    title: '공지사항',
-    route: '/contractor/notices',
+    type: 'divider',
+  },
+  {
+    key: routeToLabelMap['/contractor/notices'],
+    label: <Link href="/contractor/notices">{routeToLabelMap['/contractor/notices']}</Link>,
     icon: <NotificationOutlined />,
   },
-] as const;
+  {
+    type: 'divider',
+  },
+];
 
 export default navItems;
 
-export function getTitleByRoute(route: string) {
-  let title = '';
+export const getPageHeadingByPathname = (pathname: string) => routeToLabelMap[pathname];
+export const getMenuKeyByPathname = (pathname: string) => routeToLabelMap[pathname];
 
-  for (const page of navItems) {
-    if ('children' in page) {
-      for (const child of page.children) {
-        if (child.route === route) {
-          title = child.title;
-          break;
-        }
-      }
-    } else if (page.route === route) {
-      title = page.title;
-      break;
-    }
-  }
-
-  return title;
-}
+type Modify<T, R> = Omit<T, keyof R> & R;
+const isGroup = (item: ItemType): item is Modify<MenuItemGroupType, { key: string }> => {
+  return !!item && 'children' in item && typeof item.key === 'string';
+};
+export const allGroupKeys = navItems.filter(isGroup).map((item) => item.key);
